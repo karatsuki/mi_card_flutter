13bee9d97e52e2da36241ccfc1fe8cbfcfdec42d
karatsuki <105833425+karatsuki@users.noreply.github.com>
2022-12-08 12:47:04 +0900

    文字フォントの変更 電話番号・メールアドレスの追加

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,76 @@ class Myapp extends StatelessWidget {
           child: Column(
             children: <Widget>[
               CircleAvatar(
-                // backgroundColor: Colors.red,
                 radius: 50,
                 backgroundImage: AssetImage('images/1.png'),
               ),
               Text(
                 'Kanna',
                 style: TextStyle(
+                  fontFamily: "Pacifico",
                   fontSize: 40,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal[100],
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '020-2333-4322',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'kkk@guil.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
